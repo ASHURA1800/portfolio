33b963b02c61ca98f42cfe648d914ac70330f574
ASHURA1800 <228700200+ASHURA1800@users.noreply.github.com>
2026-07-12 14:31:21 +0530

    Add NavigationProvider and useNavigation exports

--- portfolio/app/admin/_components/nav/index.ts
+++ portfolio/app/admin/_components/nav/index.ts
@@ -1,5 +1,6 @@
 // Context + hook
 export { SidebarProvider, useSidebar } from './SidebarContext';
+export { NavigationProvider, useNavigation } from './NavigationContext';
 
 // Sidebar building blocks
 export { SidebarGroup, SidebarItem, SidebarCollapse } from './SidebarParts';
@@ -9,8 +10,11 @@ export { default as AdminSidebar } from './AdminSidebar';
 // Top chrome
 export { default as TopNavbar } from './TopNavbar';
 export { default as Breadcrumb } from './Breadcrumb';
+export { PageTitle } from './PageTitle';
+export { ThemeToggle } from './ThemeToggle';
 export { default as ProfileDropdown } from './ProfileDropdown';
 export { default as NotificationMenu } from './NotificationMenu';
 export type { Notification } from './NotificationMenu';
 export { default as CommandPalette } from './CommandPalette';
 export { default as QuickActions } from './QuickActions';
+export { useScrolled } from './useScrolled';
